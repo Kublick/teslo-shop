abd734205ae5d90445992bf9ec3e3bd9948d45ef
Max Blancarte <blancarte@gmail.com>
2022-03-11 15:05:08 -0800

    Teslo Shop Start

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import { FC } from 'react';
+
+interface Props {
+	title: string;
+	pageDescription: string;
+	imageFullUrl?: string;
+}
+
+export const ShopLayout: FC<Props> = ({
+	title,
+	pageDescription,
+	imageFullUrl,
+	children,
+}) => {
+	return (
+		<>
+			<Head>
+				<title>{title}</title>
+				<meta name="description" content={pageDescription} />
+
+				<meta name="og:title" content={title} />
+				<meta name="og:description" content={pageDescription} />
+				{imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+			</Head>
+			<nav>{/* Todo */}</nav>
+			{/* Sidebar */}
+
+			<main
+				style={{
+					margin: '80px auto',
+					maxWidth: '1440px',
+					padding: '0px 3opx',
+				}}
+			>
+				{children}
+			</main>
+
+			<footer>{/* Todo */}</footer>
+		</>
+	);
+};
