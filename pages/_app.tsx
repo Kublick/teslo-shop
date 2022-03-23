461e1aa30750b50b37586b8d90433132db82b156
Max Blancarte <blancarte@gmail.com>
2022-03-22 22:34:07 -0700

    feat: added Github auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,32 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { lightTheme } from '../themes';
-import useSWR, { SWRConfig } from 'swr';
+import { SWRConfig } from 'swr';
 import { AuthProvider, CartProvider, UiProvider } from '../context';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
-		<SWRConfig
-			value={{
-				// refreshInterval: 500,
-				fetcher: (resource, init) =>
-					fetch(resource, init).then((res) => res.json()),
-			}}
-		>
-			<AuthProvider>
-				<CartProvider>
-					<UiProvider>
-						<ThemeProvider theme={lightTheme}>
-							<CssBaseline />
-							<Component {...pageProps} />
-						</ThemeProvider>
-					</UiProvider>
-				</CartProvider>
-			</AuthProvider>
-		</SWRConfig>
+		<SessionProvider session={session}>
+			<SWRConfig
+				value={{
+					// refreshInterval: 500,
+					fetcher: (resource, init) =>
+						fetch(resource, init).then((res) => res.json()),
+				}}
+			>
+				<AuthProvider>
+					<CartProvider>
+						<UiProvider>
+							<ThemeProvider theme={lightTheme}>
+								<CssBaseline />
+								<Component {...pageProps} />
+							</ThemeProvider>
+						</UiProvider>
+					</CartProvider>
+				</AuthProvider>
+			</SWRConfig>
+		</SessionProvider>
 	);
 }
 
