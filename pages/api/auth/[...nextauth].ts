b7b46fb9649ed56384cfe4525419df7018138e94
Max Blancarte <blancarte@gmail.com>
2022-03-24 17:44:07 -0700

    feat: next auth confiig

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,54 +1,58 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import Credentials from 'next-auth/providers/credentials';
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import { db, dbUsers } from "../../../database";
 
 export default NextAuth({
-	// Configure one or more authentication providers
-	providers: [
-		GithubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
-		}),
-		Credentials({
-			name: 'Custom Login',
-			credentials: {
-				email: {
-					label: 'Correo:',
-					type: 'email',
-					placeholder: 'Correo@google.com',
-				},
-				password: {
-					label: 'Contraseña:',
-					type: 'password',
-					placeholder: 'Contraseña',
-				},
-			},
-			async authorize(credentials) {
-				console.log(credentials);
-				return { name: 'Juan', correo: 'juan@google.com', role: 'admin' };
-			},
-		}),
-	],
-	callbacks: {
-		async jwt({ token, account, user }) {
-			console.log({ token, account, user });
-			if (account) {
-				token.accessToken = account.access_token;
-				switch (account.type) {
-					case 'oauth':
-					// TODO:  crear usuario o verificar si existe en al db
-					case 'credentials':
-						token.user = user;
-						break;
-				}
-			}
+  // Configure one or more authentication providers
+  providers: [
+    Credentials({
+      name: "Custom Login",
+      credentials: {
+        email: {
+          label: "Correo:",
+          type: "email",
+          placeholder: "Correo@google.com",
+        },
+        password: {
+          label: "Contraseña:",
+          type: "password",
+          placeholder: "Contraseña",
+        },
+      },
+      async authorize(credentials) {
+        return dbUsers.checkUserEmailPassword(
+          credentials!.email,
+          credentials!.password
+        );
+      },
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, user }) {
+      console.log({ token, account, user });
+      if (account) {
+        token.accessToken = account.access_token;
+        switch (account.type) {
+          case "oauth":
+          // TODO:  crear usuario o verificar si existe en al db
+          case "credentials":
+            token.user = user;
+            break;
+        }
+      }
 
-			return token;
-		},
-		async session({ session, token, user }) {
-			session.accessToken = token.accessToken;
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      session.user = token.user as any;
 
-			return session;
-		},
-	},
+      return session;
+    },
+  },
 });
